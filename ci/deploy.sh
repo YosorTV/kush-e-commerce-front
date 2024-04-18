145926474f10203f6d02a43827f043c0ed95fb6e
MAC <liubov.smahliuk@otakoyi.com>
2024-04-18 23:22:59 +0300

    devops-0: add ci

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -7,8 +7,11 @@ docker build --build-arg NEXT_PUBLIC_API_URL="${NEXT_PUBLIC_API_URL}" \
              --build-arg NEXT_PUBLIC_URL="${NEXT_PUBLIC_URL}" \
              --build-arg NEXT_PUBLIC_STRAPI_URL="${NEXT_PUBLIC_STRAPI_URL}" \
              --build-arg NEXTAUTH_SECRET="${NEXTAUTH_SECRET}" \
-             -t kush-e-commerce-front:"${GITHUB_RUN_ID}" .
+             -t kush-e-commerce-front:"${GITHUB_RUN_ID}" . 
 
+image_id=$(docker ps | grep kush-e-commerce-front | awk '{print $1}')
+docker stop $image_id
+docker rm $image_id
 
 docker run -d --restart always -e NEXTAUTH_SECRET="${NEXTAUTH_SECRET}" \
                                -e NEXT_PUBLIC_URL="${NEXT_PUBLIC_URL}" \
