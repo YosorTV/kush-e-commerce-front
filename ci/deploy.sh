3ebd966e451f9d0cbd9b3f7053c36d7ec20bf919
MAC <liubov.smahliuk@otakoyi.com>
2024-04-24 00:19:20 +0300

    feat: add envs

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -21,6 +21,9 @@ docker run -d --restart always -e NEXTAUTH_SECRET="${NEXTAUTH_SECRET}" \
                                -e PREVIEW_SECRET="${PREVIEW_SECRET}" \
                                -e NEXT_PUBLIC_STRAPI_URL="${NEXT_PUBLIC_STRAPI_URL}" \
                                -e NEXT_PUBLIC_DATABASE_URL="postgres://strapi_admin:hepiek9eephahnohth4Eihe8Vei7wa9o@dev.kush-test.pp.ua:5432/api?synchronize=true" \
+                               -e GOOGLE_CLIENT_ID=86751189471-8dcn81dhp8eorhnr13js6t9dr32ofl5n.apps.googleusercontent.com \
+                               -e GOOGLE_CLIENT_SECRET=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 \
+                               -e JWT_SECRET=eyJhbGciOiJIUzI1NiJ9eyJjcmVhdGVkQXQiOiIyMDIzLTA5LTA5VDE3OjQwOjExLjYyM1oiLCJleHBpcmVkIjoiMjAyMy0wOS0wOVQxNzo0MDoxMS42MjNaIiwibmFtZSI6Im5hbWUiLCJ1c2VySWQiOiJpZCIsImVtYWlsIjoiZW1haWwifQ.PCwTY1WR9NnFyCzpkk7Jaf-PR-jIsMcEv_uFyiXFRDI \
                                -p 3000:3000 \
                                kush-e-commerce-front:"${GITHUB_RUN_ID}" 
 
