dfb1e6e2a8220e0a95b29e9f0dd03f7a245a63ac
evhenii.alieksnadrov <yosorit@gmail.com>
2024-10-06 23:36:15 +0300

    fix

--- components/simple/AnimatedImage/index.tsx
+++ components/simple/AnimatedImage/index.tsx
@@ -32,6 +32,7 @@ const AnimatedImage: FC<TAnimatedImage> = ({ product }) => {
       onClick={handleRedirect}
       onMouseEnter={() => setShowOverlay(true)}
       onMouseLeave={() => setShowOverlay(false)}
+      onTouchEndCapture={handleRedirect}
       className='relative h-80 sm:h-96 md:h-112'
     >
       {[img1, img2].map((img, idx) => (
